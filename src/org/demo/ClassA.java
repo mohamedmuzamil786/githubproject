ad80f8ebe1da199ecf83a0296551fe4c010dde48
mohamedmuzamil786 <moahmedmuzamil03@gmail.com>
2021-01-20 18:12:02 +0530

    git added

---
+++ src/org/demo/ClassA.java
@@ -0,0 +1,15 @@
+package org.demo;
+
+public class ClassA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c=30;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+
+}
